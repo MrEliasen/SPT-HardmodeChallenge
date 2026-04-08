6ac387fabb373222ce44879ef83bcc053f119c60
Mark <1824770+MrEliasen@users.noreply.github.com>
2026-04-08 09:38:03 +0100

    squash: fix exfil destionatio (#40)

--- client/Patches/HealthTreatmentScreenShowPatch.cs
+++ client/Patches/HealthTreatmentScreenShowPatch.cs
@@ -9,11 +9,13 @@ internal class HealthTreatmentScreenShowPatch : ModulePatch
 {
     protected override MethodBase GetTargetMethod()
     {
-        return typeof(HealthTreatmentServiceView).GetMethod(nameof(HealthTreatmentServiceView.Show), BindingFlags.Public | BindingFlags.Instance);
+        return typeof(HealthTreatmentServiceView).GetMethod(nameof(HealthTreatmentServiceView.Show),
+            BindingFlags.Public | BindingFlags.Instance);
     }
 
     [PatchPostfix]
-    protected static void PatchPostfix(DefaultUIButton ____applyButton, UpdatableToggle ____selectAllToggle, TextMeshProUGUI ____quickHealNote, TextMeshProUGUI ____costTotalField, TextMeshProUGUI ____cashInStashField)
+    protected static void PatchPostfix(DefaultUIButton ____applyButton, UpdatableToggle ____selectAllToggle,
+        TextMeshProUGUI ____quickHealNote, TextMeshProUGUI ____costTotalField, TextMeshProUGUI ____cashInStashField)
     {
         ____applyButton.GameObject.SetActive(false);
         ____selectAllToggle.gameObject.SetActive(false);
