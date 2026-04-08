c013f0cb308b6d8a0082a3fbe29fd7b61ac81da1
Mark <1824770+MrEliasen@users.noreply.github.com>
2026-04-08 08:40:19 +0100

    feat: disable post-raid treatment (#39)

---
+++ client/Patches/HealthTreatmentScreenAddTreatmentPatch.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using EFT.UI;
+using SPT.Reflection.Patching;
+ 
+namespace Vagabond.Client.Patches;
+
+internal class HealthTreatmentScreenAddTreatmentPatch : ModulePatch
+{
+    protected override MethodBase GetTargetMethod()
+    {
+        return typeof(HealthTreatmentServiceView).GetMethod("method_7", BindingFlags.Public | BindingFlags.Instance);
+    }
+
+    [PatchPrefix]
+    protected static bool PatchPrefix(HealthTreatmentServiceView __instance, ref bool ___bool_0)
+    {
+        __instance.method_10();
+        ___bool_0 = false;
+
+        return false;
+    }
+}
